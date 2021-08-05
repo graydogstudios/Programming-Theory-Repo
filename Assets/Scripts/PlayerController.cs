aa2cc597a91d8f2c3b3242d922d901a6a4086004
graydogstudios <88257038+graydogstudios@users.noreply.github.com>
2021-08-05 17:12:39 -0400

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//PLZBEINVISUALSTUDIO
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public GameObject player;
