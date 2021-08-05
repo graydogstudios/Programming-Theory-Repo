a80620b6c3f321c88cc0ccc1bfda013c27316f17
graydogstudios <88257038+graydogstudios@users.noreply.github.com>
2021-08-04 20:08:40 -0400

    Implement Title Screen
    Title screen implemented!

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+    //public T
+    public void StartGame()
+
+    {
+
+        SceneManager.LoadScene(1);
+    
+    }
+}
